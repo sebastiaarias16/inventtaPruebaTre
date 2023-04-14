9e8b94cd003d6d86ce8f1d6c85ee3a73bae7b7c1
Sebastian Arias Mulato <69696083+sebastiaarias16@users.noreply.github.com>
2023-04-14 13:40:25 -0500

    mejoras y ajustes
    se mejoro el footer y partes del header;
    se ajusto el responsive para la pagina con agregados de menu desplegable en el header

---
+++ JavaScript/menu.js
@@ -0,0 +1,9 @@
+const menuToggle = document.querySelector('.menu-toggle');
+const menuList = document.querySelector('.menu-list');
+
+menuToggle.addEventListener('click', () => {
+  menuList.classList.toggle('active');
+});
+
+
+  
